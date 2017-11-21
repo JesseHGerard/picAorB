5b89dffc82cafb872ab53ac20f3d881e88cc99dc
Jesse Gerard <jessehgerard@gmail.com>
2017-11-21 17:41:57 -0500

    add firebase tweet logging

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let bodyParser = require('body-parser');
 let path = require('path');
 let Twitter = require('twitter');
 let keys = require('./keys.js');
+let firebase = require('firebase');
 
 let app = express();
 let PORT = process.env.PORT || 3000;
@@ -12,25 +13,41 @@ app.use(bodyParser.json());
 // ___________________________________
 
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAgyDSwIZzynPdq5OvN1pVTbg60D67U91g",
+  authDomain: "picaorb-project2.firebaseapp.com",
+  databaseURL: "https://picaorb-project2.firebaseio.com",
+  projectId: "picaorb-project2",
+  storageBucket: "picaorb-project2.appspot.com",
+  messagingSenderId: "1007298219380"
+};
+firebase.initializeApp(config);
+let database = firebase.database();
+
+
+
+
+
 // start twitter stream
 let client = new Twitter(process.env.keys || keys);
 let stream = client.stream('statuses/filter', {track: '@picaorb'});
 stream.on('data', function(event) {
+  // add tweet to firebase
+  firebase.database().ref(event.id_str).set(event);
+
   console.log(`received: ${event.text}\n`);
 
   //reply to received tweet
   client.post('statuses/update', {
     //assemble reply message
-    status: `@${event.user.screen_name}`,
+    status: `@${event.user.screen_name} http://tbd.com/${event.id_str}`,
 
     in_reply_to_status_id: event.id_str,
     in_reply_to_status_id_str: event.id_str,
     in_reply_to_user_id: event.user.id,
     in_reply_to_user_id_str: event.user.id_str,
     in_reply_to_screen_name: event.user.screen_name,
-    //two lines below throw an error
-    entities: {media: event.entities.media},
-    extended_entities: {media: event.extended_entities.media},
 
 
     },  function(error, tweet, response) {
@@ -44,16 +61,6 @@ stream.on('data', function(event) {
 stream.on('error', function(error) {
   throw error;
 });
-
-// posts tweet to @picaorb account, takes any string as argument
-const postTweet = () => {
-  client.post('statuses/update', {status: 'hello there'},  function(error, tweet, response) {
-    if(error) throw error;
-    //console.log(`posted tweet: ${message}`);
-    console.log(tweet);
-
-  });
-};
 
 
 
