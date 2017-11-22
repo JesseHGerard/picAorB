a283ea40f003c70c5aa7b4184181258c8bce926f
Jesse Gerard <jessehgerard@gmail.com>
2017-11-22 16:33:11 -0500

    add individual keys and env

--- server.js
+++ server.js
@@ -40,7 +40,12 @@ if(fs.existsSync('./keys.js')){
   console.log(keys);
   startTwitterStream();
 } else {
-  client= new Twitter(process.env.keys);
+  client= new Twitter({
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token_key: process.env.access_token_key,
+    access_token_secret: process.env.access_token_secret
+  });
   startTwitterStream();
 };
 
