1e3f92146a0a19dc0d5cba95fa895a645b62aada
Jesse Gerard <jessehgerard@gmail.com>
2017-11-27 13:02:29 -0500

    server.js in local mode

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ let client;
 if(fs.existsSync('./keys.js')){
   keys = require('./keys.js');
   client= new Twitter(keys);
-  startTwitterStream();
+  //startTwitterStream();
 } else {
   client= new Twitter({
     consumer_key: process.env.consumer_key,
@@ -54,7 +54,7 @@ if(fs.existsSync('./keys.js')){
     access_token_key: process.env.access_token_key,
     access_token_secret: process.env.access_token_secret
   });
-  startTwitterStream();
+  //startTwitterStream();
 };
 
 
